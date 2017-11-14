8a1ac45d237c6348480ef7561957996c0e7c8f31
Ford Hurley <ford.hurley@gmail.com>
2017-11-14 11:48:06 -0500

    Add a WIP grammar def for THREE.ShaderMaterial
    And a non-working example of strings defined as object attributes.

--- sample.js
+++ sample.js
@@ -9,7 +9,7 @@ var vertexShader = ` // glsl
   }
 `
 
-var fragmentShader = `//glsl
+var fragmentShader = `// glsl
   varying float age;
   void main() {
     if (age < 0) { discard; }
@@ -19,6 +19,40 @@ var fragmentShader = `//glsl
 `
 
 var justAComment = `//glsl vec3 blah;`;
+
+// FIXME: these strings should be highlighted
+var program = {
+  vs: ` // glsl
+    attribute vec3 position;
+    void main() {
+      gl_Position = vec4(position, 1.0);
+    }
+  ` ,
+  fs: `//glsl
+    void main() {
+      gl_FragColor = vec4(1.0);
+    }
+  `,
+};
+
+// For this common case, we don't need the //glsl comment directive:
+var material = new THREE.ShaderMaterial({
+  vertexShader: `
+    attribute vec3 position;
+    void main() {
+      vec3 offset = vec3(1.0, 1.0); // FIXME: vec3 should be highlighted
+      gl_Position = vec4(position + offset, 1.0);
+    }
+  `,
+  fragmentShader: `
+    const float pi = 3.14;
+    uniform float u_clock;
+    void main() {
+      float brightness = sin(pi * u_clock) * 0.5 + 0.5; // FIXME: float should be highlighted
+      gl_FragColor = vec4(brightness, brightness, brightness, 1.0)
+    }
+  `,
+});
 
 function foo() {
   var x = 0;
